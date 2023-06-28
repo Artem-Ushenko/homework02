7a0d698da5a620fcd7d2f7120b8452fc41474f57
Artem_Ushenko <kingart030192kingart@gmail.com>
2023-06-28 13:36:42 +0300

    Add 4 operations to Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+
+#python01-hw % calculator.py
+#Welcome to the Calculator Program!
+
+#Please enter the first number: 10
+#Please enter the second number: 5
+
+#Please select an operation:
+# 1. Addition
+# 2. Subtraction
+# 3. Multiplication
+# 4. Division
+#
+# Enter your choice (1-4): 3
+
+#The result of multiplication is: 50
+
+def addition (a, b) :
+    return a+b
+
+def substraction (a, b) :
+    return a-b
+
+def multiplication (a, b) :
+    return a*b
+
+def division (a, b) :
+    return a/b
+
+print(addition(5, 5))
