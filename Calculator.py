9e7945bf30e108dc1a3ffe3a36541c81e46a3dc8
Artem_Ushenko <kingart030192kingart@gmail.com>
2023-06-30 20:15:29 +0300

    More bugs was fixed. New Version2 of Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,51 +1,49 @@
-# Create a basic calculator program using Python.
-#
-# The program should allow the user to perform simple arithmetic operations on two numbers.
-#
-# Requirements:
-#
-# Prompt the user to enter two numbers.
-# Prompt the user to select an operation from the following options:
-# addition
-# subtraction
-# multiplication
-# division.
+def plus(number1, number2):
+    return number1 + number2
+
+def minus(number1, number2):
+    return number1 - number2
+
+def umnozhenie(number1, number2):
+    return number1 * number2
+
+def podelit(number1, number2):
+    if number2 == 0:
+        print("Zero Division Error !!!")
+        return None
+    return number1 / number2    
 
 print("Welcome to the Calculator Program!")
 
-a = float(input("Please enter the first number:"))
-b = float(input("Please enter the second number:"))
+try:
+    number1 = int(input("Please enter the first number: "))
+    number2 = int(input("Please enter the second number: "))
 
-def addition(a, b):
-    return a + b
+    operation = int(input("""Please select an operation:
+        
+        1. Addition
+        2. Subtraction
+        3. Multiplication
+        4. Division
 
-def substraction(a, b):
-    return a - b
 
-def multiplication(a, b):
-    return a * b
+    Enter your choice (1-4): """))
 
-def division(a, b):
-    return a / b
+    if operation == 1:
+        print("The result of addition is: ", plus(number1, number2))
 
-c = int(input("""Please select an operation:
-1. Addition
-2. Subtraction
-3. Multiplication
-4. Division 
+    elif operation == 2:
+        print("The result of subtraction is: ", minus(number1, number2))
 
-Enter your choice (1-4): """))
+    elif operation == 3:
+        print("The result of multiplication is: ", umnozhenie(number1, number2))
 
-if c == 1:
-    print("The result of addition is:", addition(a, b))
-elif c == 2:
-    print("The result of substraction is:", substraction (a, b))
-elif c == 3:
-    print("The result of multiplication is:", multiplication (a, b))
-elif c == 4 :
-    if b == 0 :
-        print("Error: Division by zero. Please restart the program and input not 0 second number")
+    elif operation == 4:
+        result = podelit(number1, number2)
+        if result is not None:
+            print("The result of division is: ", result)
     else:
-        print("The result of division is:", division (a, b))
-else :
-    print("Error: only 1 to 4 must be chosen")
+        print("Operation was selected incorrectly !!! Your choice must be 1-4 ")       
+
+except ValueError:
+    print("Not a number !!! Restart the program and input only numbers.")
